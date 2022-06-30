a8eb998f02ea1253951ec808696d29fbce6bee13
golovkinen <golovkinen@yandex.ru>
2022-06-30 22:21:28 +0300

    New Controllers Users 3

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 
@@ -11,7 +10,7 @@ import java.time.LocalDate;
 //@AllArgsConstructor
 public class Film {
 
-    private int id = 0;
+    private Integer id;
     @NotBlank
     @NonNull
     private String name;
